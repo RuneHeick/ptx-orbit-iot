f0c1d053ad12e2bc7a286e21613fc6a1ea276b61
RuneHeick <hteml@hotmail.com>
2020-11-25 09:50:36 +0100

    test change

--- OrbitIOT.ts
+++ OrbitIOT.ts
@@ -1,1 +1,30 @@
+namespace Orbit_IoT {
 
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 0) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    /**
+    * Initialize ESP8266 module 
+    */
+    //% block="set ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
+    //% tx.defl=SerialPin.P8
+    //% rx.defl=SerialPin.P12
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_password weight=100
+    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+CWMODE=1") // set to STA mode
+        basic.pause(1000)
+    }
+
+
+
+}
