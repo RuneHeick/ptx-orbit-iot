dd7645196c42344d272df2a07e1800a47fab3771
RuneHeick <hteml@hotmail.com>
2020-11-25 10:21:15 +0100

    first draft - connection

--- OrbitIOT.ts
+++ OrbitIOT.ts
@@ -1,20 +1,42 @@
 namespace Orbit_IoT {
 
+    const wifi_ssid :string  = "OrbitWifi"
+    const wifi_pw :string  = "Orbit1234"
+
+    const endpoint :string = "34.66.72.29"
+    const port :string = "5000"
+
+    let cloud_connected: boolean = false
+
     // write AT command with CR+LF ending
     function sendAT(command: string, wait: number = 0) {
         serial.writeString(command + "\u000D\u000A")
         basic.pause(wait)
     }
 
-    /**
-    * Initialize ESP8266 module 
-    */
-    //% block="set ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
-    //% tx.defl=SerialPin.P8
-    //% rx.defl=SerialPin.P12
-    //% ssid.defl=your_ssid
-    //% pw.defl=your_password weight=100
-    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+    function waitForResponse(exspect: string, timeout : number = 10000): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200)
+                serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes(exspect)) {
+                result = true
+                break
+            }
+            else if (input.runningTime() - time > timeout) {
+                break
+            }
+        }
+        return result
+    }
+
+    
+    //Initialize ESP8266 module 
+    function setupESP8266(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
         serial.redirect(
             tx,
             rx,
@@ -23,6 +45,41 @@ namespace Orbit_IoT {
         sendAT("AT+RESTORE", 1000) // restore to factory settings
         sendAT("AT+CWMODE=1") // set to STA mode
         basic.pause(1000)
+    }
+
+    function connectWifi(ssid: string, pw: string) : boolean {
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
+        return waitForResponse("WIFI GOT IP")
+    }
+
+    function connectOrbitCloud() 
+    {
+        let cmd = "AT+CIPSTART=\"TCP\",\"" + endpoint + "\","+ port
+        sendAT(cmd)
+        return waitForResponse("CONNECT")
+    }
+
+    //% block="Setup OrbitLab Cloud"
+    export function setupForCloud()
+    {
+        if(cloud_connected == false)
+        {
+            setupESP8266(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+            if(connectWifi(wifi_ssid, wifi_pw) && connectOrbitCloud())
+            {
+                cloud_connected = true;
+            }
+        }
+    }
+
+    //% block="Cloud Connected %state" weight=70
+    export function wifiState(state: boolean) : boolean {
+        if (cloud_connected == state) {
+            return true
+        }
+        else {
+            return false
+        }
     }
 
 
